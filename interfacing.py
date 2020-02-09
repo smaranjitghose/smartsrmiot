f1ef9fc903568206827a4a49f4b8fe647f005aab
swat1998 <saswatnayak1998@gmail.com>
2020-02-09 21:48:09 +0530

    update 1.1

--- interfacing.py
+++ interfacing.py
@@ -1,12 +1,39 @@
 import time
 import serial
+import mysql.connector as mysql
 
-data = serial.Serial(
-	port=dev/ttyS0
-	baudrate=9600
-	parity=serial.PARITY_NONE
-	stopbyte=serial.SOTPBBITS_ONE
-	bytesize=serial.EIGHTBIT)
+db=mysql.conection(
+	host=''
+	user=''
+	passwd=''
+	database='')
 
-while 1:
-	x = data.read(12)
+corsor=db.corsor()
+
+class timer:
+	__init__(self)
+		t=300
+		while t!=0:
+			time.sleep(1)
+			t=t-1
+			return t
+
+class id_pull:
+	__init__(self)
+		data = serial.Serial(
+			port=dev/ttyS0
+			baudrate=9600
+			parity=serial.PARITY_NONE
+			stopbyte=serial.SOTPBBITS_ONE
+			bytesize=serial.EIGHTBIT)
+
+		while 1:
+			x = data.read(12)
+			if x.isnumeric == true:
+				#timer_initialize
+				timer()
+				#sql_code
+				corsor.execute('')
+			elif x.isalnum == true && timer()!=0:
+				#sql_code
+				corsor.execute('')
