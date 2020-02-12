3b28adec1bd51728d792e8e6f1f86cc6dd91a779
Smaranjit Ghose <46641503+smaranjitghose@users.noreply.github.com>
2020-02-13 04:04:40 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo 'installing sql connector...'
-sudo apt-get install mysql-python
-sudo apt-get install python-mysql.connector
-sudo apt-get install pymysql
-sudo apt-get install mysqldb
-echo '#sql connector installed successfully#'
